2e428c9a197a96be20c93262d2c1773396de66be
William Jiang <wj2065@nyu.edu>
2026-03-08 16:29:48 -0400

    model

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -19,24 +19,20 @@ Make sure to return pure JSON.
 The front should be a question.
 The back should be the answer to the question.
 `;
-
 export async function POST(req) {
-  console.log("Key exists:", !!process.env.OPENROUTER_API_KEY);
   const openai = new OpenAI({
     baseURL: "https://openrouter.ai/api/v1",
     apiKey: process.env.OPENROUTER_API_KEY,
   });
   const data = await req.text();
+  console.log("🚀 ~ POST ~ data:", data);
   const completion = await openai.chat.completions.create({
+    model: "qwen/qwen3-vl-235b-a22b-thinking",
     messages: [
       { role: "system", content: systemPrompt },
       { role: "user", content: data },
     ],
-    model: "liquid/lfm-2.5-1.2b-thinking:free",
-    response_format: { type: "json_object" },
   });
-
   const flashcards = JSON.parse(completion.choices[0].message.content);
-
   return NextResponse.json(flashcards.flashcards);
 }
